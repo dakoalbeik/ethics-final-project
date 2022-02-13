8354cc5acfa8cfa36c04d4aa23a25fb161eba451
Dako Albeik <dako.albeik@gmail.com>
2022-02-13 12:54:44 -0600

    Grab HTML elements in JS

---
+++ main.js
@@ -0,0 +1,8 @@
+// References to DOM Elements
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const book = document.querySelector("#book");
+
+const paper1 = document.querySelector("#p1");
+const paper2 = document.querySelector("#p2");
+const paper3 = document.querySelector("#p3");
